41714de4b4d9430b6ac815d6fcaa01d78606b03c
Walany Costa <walanybnegro@gmail.com>
2025-01-01 14:20:47 +0100

    feat: add layout screen perfile

--- src/app/(employee)/settings/index.tsx
+++ src/app/(employee)/settings/index.tsx
@@ -1,21 +1,98 @@
 import { useAuth } from "@/context/auth-context";
 import { supabase } from "@/lib/supabase";
-import { Button } from "react-native";
-import { View, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Text, TouchableOpacity } from "react-native";
+import { Container } from "@/components/layout";
+import { ArrowLeft } from "phosphor-react-native";
+import { router } from "expo-router";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/Avatar";
+import {
+	Image,
+	CaretRight,
+	Lock,
+	PaintRoller,
+	SignOut,
+} from "phosphor-react-native";
+import { Switch } from "@/components/ui/Switch";
+import { useState } from "react";
 
 export default function Home() {
 	const { setAuth } = useAuth();
+	const [isEnabled, setIsEnabled] = useState(false);
 
 	async function signOut() {
 		await supabase.auth.signOut();
 		setAuth(null);
 	}
 
+	function handleBack() {
+		router.back();
+	}
+
 	return (
-		<SafeAreaView>
-			<Text className="">Hello react native</Text>
-			<Button title="signout" onPress={() => signOut()} />
-		</SafeAreaView>
+		<Container>
+			<TouchableOpacity
+				onPress={handleBack}
+				className="w-8 h-8 mt-3 mb-5 justify-center items-start"
+			>
+				<ArrowLeft color="#4B5563" size={24} />
+			</TouchableOpacity>
+
+			<View className="gap-3 justify-center items-center">
+				<Avatar className="rounded-md w-16 h-16 border border-input">
+					<AvatarImage source={require("@/assets/logo.png")} />
+					<AvatarFallback>MF</AvatarFallback>
+				</Avatar>
+
+				<View>
+					<Text className="font-heading text-center text-xl mb-2">
+						Marcos Ferreira
+					</Text>
+					<Text className="text-center text-sm text-gray-500">
+						marcosferreira@mail.com
+					</Text>
+				</View>
+			</View>
+
+			<View className="mt-9 gap-3 justify-center ">
+				<Text className="text-gray-400 text-sm ml-3">Usuario</Text>
+				<View className="border border-input p-6 gap-2 rounded-md">
+					<TouchableOpacity className="flex-row item-center justify-between border-b border-input py-2">
+						<View className="flex-row gap-2 items-center">
+							<Image color="#4B5563" size={20} />
+							<Text className="text-xs text-gray-600">Alterar imagem</Text>
+						</View>
+
+						<CaretRight color="#4B5563" size={16} />
+					</TouchableOpacity>
+
+					<TouchableOpacity className="flex-row item-center justify-between border-b border-input py-2">
+						<View className="flex-row gap-2 items-center">
+							<Lock color="#4B5563" size={20} />
+							<Text className="text-xs text-gray-600">Alterar senha</Text>
+						</View>
+
+						<CaretRight color="#4B5563" size={16} />
+					</TouchableOpacity>
+
+					<TouchableOpacity className="flex-row item-center justify-between">
+						<View className="flex-row gap-2 items-center">
+							<PaintRoller color="#4B5563" size={20} />
+							<Text className="text-xs text-gray-600">
+								Alterar tema (light/dark)
+							</Text>
+						</View>
+
+						<Switch onValueChange={setIsEnabled} value={isEnabled} />
+					</TouchableOpacity>
+				</View>
+
+				<TouchableOpacity className="flex-row item-center justify-between mt-5 py-2 border border-input rounded-md">
+					<View className="flex-row gap-2 items-center ml-5">
+						<SignOut color="#4B5563" size={20} />
+						<Text className="text-xs text-gray-600">Logout</Text>
+					</View>
+				</TouchableOpacity>
+			</View>
+		</Container>
 	);
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+  light: {
+    background: '#E5E7EB',
+    foreground: '#7C3AED',
+  },
+  dark: {
+    background: 'hsl(224, 71% 4%)',
+    foreground: 'hsl(213, 31% 91%)',
+  },
+};
