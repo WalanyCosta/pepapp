4b9a2fbd53514f7ac2fdf6b3ff7a49768e573de7
Walany Costa <walanybnegro@gmail.com>
2025-02-06 11:41:14 +0100

    chore: move environment variables to file env

--- src/lib/create-user.ts
+++ src/lib/create-user.ts
@@ -1,3 +1,5 @@
+import { supabaseAnonKey } from "./config-supabase";
+
 export async function createUser(data: any){
   let user = null;
   let error = null;
@@ -6,7 +8,7 @@ export async function createUser(data: any){
     user = await fetch("https://iborvasrdopkfbmwuywt.supabase.co/auth/v1/signup", {
       method: "POST",
       headers: {
-        "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlib3J2YXNyZG9wa2ZibXd1eXd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU1NjAwNDgsImV4cCI6MjA1MTEzNjA0OH0.ZvcpaPbqhn4B6jkGEleMbxZu9dWrnIxZmooDs_9QLLU",
+        "apikey": supabaseAnonKey ?? "",
         "Content-Type": "application/json"
       },
       body: JSON.stringify(data)
